f85d760913641f0755de1b4f8b8198fce9de71ca
habibitgithub <117856788+habibitgithub@users.noreply.github.com>
2022-11-28 14:20:48 +0500

    work done

---
+++ LabTask5.java
@@ -0,0 +1,29 @@
+class LabTask5 {
+public static void main(String[] args)
+{
+// \t: It gives a tab between two words.
+  System.out.println("Good Morning\t Easy ");
+
+
+// \n: This escape sequence is for a new line.
+  System.out.println("Good Morning \n Easy ");
+  System.out.print("*\n**\n***\n");
+
+
+  // \r: It moves the output point back to the starting of the line without moving down a line (generally).
+  System.out.println("Good Morning \r Easy ");
+
+  // \': It is used for printing a single quotation mark on the text string.
+  System.out.println("Good Morning \'Easy\'");
+
+  // \":  It is used for printing a double quotation mark on the text string.
+  System.out.println("Good Morning \"Easy\"");
+
+
+// \\: It is used for printing a backslash on the text string.
+  System.out.println("Good Morning \\Easy\\");
+
+
+
+  }
+}
---
+++ LabTask7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class LabTask7
+{
+ public static void main(String[] args)
+ {
+  Scanner in=new Scanner(System.in);
+  String username,password;//variable declaration
+  System.out.println("Enter username");
+  username=in.next();//input username
+  System.out.println("Enter password");
+  password=in.next();//input password
+  if(username.equals("easy@gmail.com") && password.equals("12345"))
+      System.out.println("Login Success");
+  else
+         System.out.println("Wrong username or password");
+
+
+
+
+    }
+}
